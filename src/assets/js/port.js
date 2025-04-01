1efd2325df6fe2dc8a123e0765726743126800ef
park-jjoon <mymaengjoon@gmail.com>
2025-04-01 17:37:05 +0900

    포트폴리오 영역

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,9 +1,26 @@
 
 
-
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 // 내보내기
 export function port(){
-    console.log(port);
-    
+    gsap.registerPlugin(ScrollTrigger);
+
+    const horSection = gsap.utils.toArray('.port__item');
+
+    gsap.to(horSection, {
+        xPercent: -120 * (horSection.length),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#port",
+            start:"top 100px",
+            end: "+=2500",
+            pin: true,
+            markers: true,
+            scrub: 1,
+            aniticipatePin: 1
+        }
+    });
+
 }
