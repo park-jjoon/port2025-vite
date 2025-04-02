3a2feaa7dd3bc6a723980fa26ca6c120c4de3ff2
park-jjoon <mymaengjoon@gmail.com>
2025-04-02 09:07:30 +0900

    '완료'

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -4,6 +4,20 @@
 
 // 내보내기
 export function link(){
-    console.log(link);
+    document.querySelectorAll('a[href^="#"]').forEach((link) => {
+        link.addEventListener('click',function (e){
+            e.preventDefault();
+            
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+
+
+            if(targetElement){
+                targetElement.scrollIntoView({behavior: 'smooth'});
+            }
+            console.log(targetElement);
+            
+        });
+    });
     
 }
--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,9 +1,24 @@
 
+import Lenis from "@studio-freight/lenis";
 
 
 
 // 내보내기
 export function smooth(){
-    console.log(smooth);
+    
+    const lenis = new Lenis({
+        duration:1,
+        
+    })
+
+    lenis.on('scroll', (e) => {
+        console.log(e);
+    });
+
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
     
 }
